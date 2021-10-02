6ea0ec366d3b55e95c377a36d5bb6cd7f320eced
Michael Altreuter <maltreuter@Michaels-MacBook-Air.local>
2021-10-02 12:50:21 -0500

    add skeleton for symbol checking

--- shell.c
+++ shell.c
@@ -2,27 +2,62 @@
 
 extern int get_command();
 
-int do_command(char **command) {
+// run commands (recursively)
+int do_command(char **command, int pipe, int input, int ouput) {
   pid_t child_pid;
   int status;
   int result;
   int i;
+  char **next_command;
 
-  printf("Running command: ");
-  for(i = 0; command[i] != NULL; i++) {
-    printf("%s ", command[i]);
+  if(command[0] != NULL) {
+      printf("Running command: ");
+      for(i = 0; command[i] != NULL; i++) {
+        printf("%s ", command[i]);
+      }
+      printf("\n");
+
+      // Check for input/output/append
+
+      // Check for ampersand for backgrounding
+
+      // Check for pipes
+
+      child_pid = fork();
+      if(child_pid == 0) {
+          execvp(command[0], command);
+      }
+
+      result = waitpid(child_pid, &status, 0);
+
+      return result;
   }
-  printf("\n");
+  return 0;
+}
 
+int ampersand(char **command) {
+    printf("ampersand\n");
+    return 0;
+}
 
-  child_pid = fork();
-  if(child_pid == 0) {
-      execvp(command[0], command);
-  }
+int check_pipe(char **command) {
+    printf("pipe\n");
+    return 0;
+}
 
-  result = waitpid(child_pid, &status, 0);
+int input_redir(char **command, char **input_filename) {
+    printf("input redir\n");
+    return 0;
+}
 
-  return result;
+int output_redir(char **command, char **output_filename) {
+    printf("output redir\n");
+    return 0;
+}
+
+int append(char **command, char **output_filename) {
+    printf("append\n");
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
--- shell.h
+++ shell.h
@@ -9,6 +9,11 @@
 #define MAX_ARG_COUNT 128
 #define SHELL_PID getpid()
 
-int do_command(char **command);
+int do_command(char **command, int pipe, int input, int output);
+int ampersand(char **command);
+int check_pipe(char **command);
+int input_redir(char **command, char **input_filename);
+int output_redir(char **command, char **output_filename);
+int append(char **command, char **output_filename);
 
 #endif //SHELL_H
